01328ad0d48f76222d3822385c2aac5ad8f442db
ndohertyjr <ndohertyjr@gmail.com>
2022-08-30 13:52:27 -0400

    Completed refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,7 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getCurrentQuestion(quizBrain.questionIndex),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -50,9 +55,14 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.resolveWith(
+                      (states) => Colors.white),
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) => Colors.green),
+                  textStyle:
+                      MaterialStateProperty.resolveWith((states) => null)),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -62,6 +72,10 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  quizBrain.verifyAnswer(true);
+                  quizBrain.getNextQuestion();
+                });
               },
             ),
           ),
@@ -69,8 +83,14 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.resolveWith(
+                      (states) => Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.resolveWith((states) => Colors.red),
+                  textStyle:
+                      MaterialStateProperty.resolveWith((states) => null)),
               child: Text(
                 'False',
                 style: TextStyle(
@@ -80,11 +100,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  quizBrain.verifyAnswer(false);
+                  quizBrain.getNextQuestion();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: quizBrain.scoreKeeper,
+        )
       ],
     );
   }
